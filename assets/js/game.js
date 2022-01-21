8eeaec498aa4902eea8ef9056514ee876378b735
Chris <christina.hernandez580@gmail.com>
2022-01-20 20:58:07 -0600

    first battle has started

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,11 @@
 var playerName = window.prompt("What is your robot's name?");
-var playerHealth = 100;
+var playerHealth = 11;
 var playerAttack = 10;
 
 // You can also log multiple values at once like this console.lgo(playerName, playerAttack, playerHealth); 
 
 var enemyName = "Roborto";
-var enemyHealth = 50;
+var enemyHealth = 9;
 var enemyAttack = 12; 
 
 var fight = function() {
@@ -13,12 +13,20 @@ var fight = function() {
     window.alert("Welcome to Robot Gladiators!");
 
     //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' varialble
-    enemyHealth = enemyHealth - playerAttack;
+    playerHealth = playerHealth - enemyAttack;
 
     //Log a resulting message to the console so we know that it worked.
     console.log(
-        playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining" 
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining." 
     );
+
+    //check enemy's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 
     //Subtract the value of 'enemyAttack' from the value of 'playerHealth' variable.
 
